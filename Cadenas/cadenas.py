973020afeb79ed56577a66d2265eb4d9302c5d77
dafnemus <dafnemus@gmail.com>
2021-03-02 09:50:08 -0300

    manejo de cadenas y uso de print

---
+++ Cadenas/cadenas.py
@@ -0,0 +1,31 @@
+# pylint: disable=missing-docstring
+
+# Cadenas:
+
+NOMBRE = 'Dafne'
+CARRERA = 'Edición'
+
+print(NOMBRE)
+print(CARRERA)
+
+# tipo de dato
+print(type(NOMBRE))
+
+# print: tabulaciones y saltos de linea:
+
+# tab
+print('hola mundo \t adios mundo')
+# salto de linea
+print('hola mundo \n adios mundo')
+
+# cadena sin procesar con print:
+
+print('c:\nombre\nombre_2')
+print(r'c:\nombre\nombre_2')
+
+# cadena en varia lineas:
+print('''Línea 1
+línea 2
+línea 3
+etc
+''')
