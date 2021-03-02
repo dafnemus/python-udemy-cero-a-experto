b6133a15482f421df4f840ef3e26b0669afa9936
dafnemus <dafnemus@gmail.com>
2021-03-02 10:57:23 -0300

    manejo de booleanos

---
+++ Booleanos/booleanos.py
@@ -0,0 +1,25 @@
+# pylint: disable=missing-docstring
+
+# Booleanos: True or False.
+
+SUMA = 1 + 1
+
+# True
+print(SUMA == 2)
+
+# False
+print(SUMA == 3)
+
+# True and True
+
+verdad = (SUMA == 2) and (SUMA + 1 == 3)
+
+print(verdad)
+
+# Tipo de Dato.
+print(type(verdad))
+
+# True and False
+
+falsedad = (SUMA == 2) and (SUMA + 2 == 3)
+print(falsedad)
