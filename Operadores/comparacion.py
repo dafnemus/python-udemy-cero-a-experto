d81a3e64d93edddd23b0ee001066ff09d442757a
dafnemus <dafnemus@gmail.com>
2021-03-02 12:49:34 -0300

    operadores de comparacion y logica

---
+++ Operadores/comparacion.py
@@ -0,0 +1,25 @@
+# pylint: disable=missing-docstring
+
+# Operadores de Comparación:
+
+# Igualdad:
+igualdad = 2 == 5
+print(f'2 es igual a 5: {igualdad}')
+
+# Desigualdad:
+desigualdad = 2 != 3
+print(f'2 es desigual a 3: {desigualdad}')
+
+# Magnitudes:
+# - mayor y mayor-igual:
+resultado = 2 > 3
+print(f'2 es mayor a 3: {resultado}')
+resultado = 2 >= 3
+print(f'2 es mayor o igual a 2: {resultado}')
+
+# - menor y menor-igual:
+resultado = 2 < 3
+print(f'2 es menor a 3: {resultado}')
+
+resultado = 3 <= 2
+print(f'3 es menor a 2: {resultado}')
