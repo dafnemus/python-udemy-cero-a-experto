57dd163b5ef13264d4d9d485e9a8495acf07256a
dafnemus <dafnemus@gmail.com>
2021-03-02 12:00:02 -0300

    operadores aritmeticos

---
+++ Operadores/aritmeticos.py
@@ -0,0 +1,39 @@
+# pylint: disable=missing-docstring
+
+# Operadores Aritméticos:
+
+# Adición:
+
+SUMA = 1 + 1
+print(F'Resultado suma:{SUMA}')
+
+# Concatenación:
+CADENA = 'hola' + 'Mundo'
+print(f'Concatenación: {CADENA}')
+
+# Substracción:
+RESTA = 2 - 1
+print(f'Resultado resta: {RESTA}')
+
+# Multiplicaión:
+MULTIPLICACION = 2 * 2
+print(f'Resultado multiplicaciónf: {MULTIPLICACION}')
+
+PALABRA = 'hola ' * 2
+print(f'palabra multiplicada: {PALABRA}')
+
+# División:
+DIVISION = 10 / 2
+print(f'Resultado división: {DIVISION}')
+
+# División entera truncada
+ENTERO = 10 // 2
+print(f'Resultado div-entero: {ENTERO}')
+
+# Módulo:
+MODULO = 5 % 2
+print(f'Módulo: {MODULO}')
+
+# Exponenciación:
+POTENCIA = 2 ** 4
+print(f'Potencia: {POTENCIA}')
