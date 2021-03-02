7a180c9e726ba39fded96d70096249bb05cc609c
dafnemus <dafnemus@gmail.com>
2021-03-02 12:33:21 -0300

    operadores logicos, and, or , not

---
+++ Operadores/logicos.py
@@ -0,0 +1,31 @@
+# pylint: disable=missing-docstring
+
+# Operadores Lógicos:
+
+# AND:
+VERDAD = True and True
+print(VERDAD)
+
+FALSEDAD = False and False
+print(FALSEDAD)
+
+FALSEDAD = True and False
+print(FALSEDAD)
+
+# OR:
+VERDAD = True or False
+print(VERDAD)
+
+VERDAD = True or True
+print(VERDAD)
+
+FALSEDAD = False or False
+print(FALSEDAD)
+
+
+# NOT, negación:
+FALSEDAD = not True
+print(FALSEDAD)
+
+VERDAD = not False
+print(VERDAD)
