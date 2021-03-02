1b093a54487fe80e4131bd0d40f8a46c29770724
dafnemus <dafnemus@gmail.com>
2021-03-02 12:08:53 -0300

    operadores de asignacion

---
+++ Operadores/asignacion.py
@@ -0,0 +1,48 @@
+# pylint: disable=missing-docstring
+
+# Opradores de Asignación:
+
+# Adición += :
+
+SUMA = 1
+SUMA += 3
+print(F'Resultado suma:{SUMA}')
+
+# Concatenación +=:
+CADENA = 'hola'
+CADENA += 'Mundo'
+print(f'Concatenación: {CADENA}')
+
+# Substracción -=:
+RESTA = 2
+RESTA -= 2
+print(f'Resultado resta: {RESTA}')
+
+# Multiplicaión *=:
+MULTIPLICACION = 2
+MULTIPLICACION *= 6
+print(f'Resultado multiplicaciónf: {MULTIPLICACION}')
+
+PALABRA = 'hola '
+PALABRA *= 3
+print(f'palabra multiplicada: {PALABRA}')
+
+# División /=:
+DIVISION = 10
+DIVISION /= 2
+print(f'Resultado división: {DIVISION}')
+
+# División entera truncada //=:
+ENTERO = 10
+ENTERO //= 2
+print(f'Resultado div-entero: {ENTERO}')
+
+# Módulo %=:
+MODULO = 5
+MODULO %= 2
+print(f'Módulo: {MODULO}')
+
+# Exponenciación:
+POTENCIA = 2
+POTENCIA **= 6
+print(f'Potencia: {POTENCIA}')
