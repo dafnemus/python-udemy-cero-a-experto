fc777b53fb51ff5ff2362fa4392ed9937df648f6
dafnemus <dafnemus@gmail.com>
2021-03-03 17:05:55 -0300

    indices y slicing

---
+++ Indices/indices.py
@@ -0,0 +1,21 @@
+# pylint: disable=missing-docstring
+
+# Indices y slicing
+
+# Ejemplo con cadena:
+TEXTO = 'Hola Mundo'
+print(TEXTO)
+# indice
+print(TEXTO[0], TEXTO[1], TEXTO[2])
+
+# indice inverso
+print(TEXTO[-3], TEXTO[-2], TEXTO[-1])
+
+# slicing:
+print(TEXTO[0:4])
+
+# con omisión:
+print(TEXTO[:3])
+
+# inverso
+print(TEXTO[-6:2])
