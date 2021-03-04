c847cbc73d14ca01bde38c3e2849317d4dbfc399
dafnemus <dafnemus@gmail.com>
2021-03-04 13:15:36 -0300

    manejo de listas 1

---
+++ Listas/listas_1.py
@@ -0,0 +1,30 @@
+# pylint: disable=missing-docstring
+
+# Listas:
+productos = ['Huevos', 'Leche', 'Queso']
+precios = [1.5, 2, 5, 10]
+
+# tipo de dato:
+print(productos, type(productos))
+
+print(f'Los {productos[0]} cuestan ${precios[0]}')
+
+# Mutabilidad: Aumento de precio.
+precios[0] = 15
+print(f'Los {productos[0]} ahora cuestan ${precios[0]}')
+
+# lista con range(a, b, in):
+#   - a = limite inferior de la secuencia de números
+#   - b = limite superior(se excluye)
+#   - in = incremento(opcional)
+numeros = range(0, 20, 2)
+print(numeros)
+lista = list(numeros)
+print(lista)
+
+# añadir elementos:
+
+lista.append(20)
+print(lista)
+
+
