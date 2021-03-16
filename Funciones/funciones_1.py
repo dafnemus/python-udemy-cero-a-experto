f4f5f32df160439bfdf30ae81c1c65ccae149993
dafnemus <dafnemus@gmail.com>
2021-03-16 10:58:22 -0300

    funciones basicas y return

---
+++ Funciones/funciones_1.py
@@ -0,0 +1,26 @@
+# pylint: disable=missing-docstring
+
+# Funciones:
+
+# Saludo Mundo:
+
+def saludar() -> str:
+    print('Hola Mundo')
+
+saludar()
+
+def mostrar_tabla_del_5() -> str:
+    for i in range(10):
+        print(f'5 * {i} = {5*i}')
+
+mostrar_tabla_del_5()
+
+# Retorno de valores:
+def sumar() -> int:
+    return 2 + 2
+
+def listar() -> list:
+    return [1, 2, 3, 4, 5]
+
+# print un return
+print(listar())
